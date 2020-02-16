a95cb6e7116f20f462b932bfbfdfd0e06a052357
Yelyzaveta <riabovaYelyzaveta@gmail.com>
2020-02-16 23:34:15 +0200

    Created first page view

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+function Footer() {
+    return (
+        <div className='footer'>
+            <h2>bottom website</h2>
+        </div>
+    )
+}
+
+export default Footer
